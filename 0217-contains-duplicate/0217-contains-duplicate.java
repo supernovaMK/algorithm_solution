c1e0b1ae6f7ea383bef5b4483dfd4d6ee95ab961
김민기 <115217054+supernovaMK@users.noreply.github.com>
2026-07-15 11:50:53 +0900

    Time: 30 ms (5.51%), Space: 92.5 MB (75.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        if(nums.length==set.size()){
+            return false;
+        }
+
+        return true;
+    }
+}
